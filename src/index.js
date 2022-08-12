ec7909e3985600c6da87147b133d62b8afcc24b9
benjamalegni <lukabenjaminmalegni@gmail.com>
2022-08-12 15:59:28 -0300

    backend ready

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import express from 'express'
+import {dirname, join} from 'path'
+import {fileURLToPath} from 'url'
+
+import indexRoutes from './routes/index.js'
+
 const app = express()
 
-app.get('/', (req, res) => res.send('hola bobo'))
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.use(express.static(__dirname + '/views'));
+
+
+app.use(indexRoutes)
+
+app.use(express.static(join(__dirname, '/public')))
 
 app.listen(3000)
-console.log('server listening port 3000'.green)
+console.log('server listening port 3000')
